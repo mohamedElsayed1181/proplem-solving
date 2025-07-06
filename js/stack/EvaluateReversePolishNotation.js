b7d04e7f347bef7bfac01ceb3d6709d76be981a3
mohamedElsayed1181 <mohamedelsayed908070@gmail.com>
2025-07-06 15:59:44 +0300

    evlouate ReversePolish Notaion

---
+++ js/stack/EvaluateReversePolishNotation.js
@@ -0,0 +1,59 @@
+// You are given an array of strings tokens that represents a valid arithmetic expression in Reverse Polish Notation.
+
+// Return the integer that represents the evaluation of the expression.
+
+// The operands may be integers or the results of other operations.
+// The operators include '+', '-', '*', and '/'.
+// Assume that division between integers always truncates toward zero.
+// Example 1:
+
+// Input: tokens = ["1","2","+","3","*","4","-"]
+
+// Output: 5
+
+// Explanation: ((1 + 2) * 3) - 4 = 5
+
+
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+
+        for (let token of tokens) {
+            if (!isNaN(token)) {
+                stack.push(Number(token));
+            } else {
+                const b = stack.pop();
+                const a = stack.pop();
+                let result;
+                    
+                switch (token) {
+                    case '+':
+                        result = a + b;
+                        break;
+                    case '-':
+                        result = a - b;
+                        break;
+                    case '*':
+                        result = a * b;
+                        break;
+                    case '/':
+                        result = Math.trunc(a / b); 
+                        break;
+                }
+
+                stack.push(result);
+            }
+        }
+
+        return stack[0];
+    }
+}
+
+
+
+const s = new Solution();
+console.log(s.evalRPN(["1", "2", "+", "3", "*", "4", "-"])); 
